f279776a01a9409eff539644a194800b899695fb
Juan Jose Alvarez <juan.pfalvarez@gmail.com>
2023-03-08 14:00:51 -0600

    Creaciond de la conexion a la base de datos

--- Proyecto_Final_Sistema_Reservaciones/Pages/Mis_Reservaciones.aspx.designer.cs
+++ Proyecto_Final_Sistema_Reservaciones/Pages/Mis_Reservaciones.aspx.designer.cs
@@ -13,23 +13,5 @@ namespace Proyecto_Final_Sistema_Reservaciones.Pages
 
     public partial class Mis_Reservaciones
     {
-
-        /// <summary>
-        /// Control Label1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
-
-        /// <summary>
-        /// Control Label2.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label2;
     }
 }

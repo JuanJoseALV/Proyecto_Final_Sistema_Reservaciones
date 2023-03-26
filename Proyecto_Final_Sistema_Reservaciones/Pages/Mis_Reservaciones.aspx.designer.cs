68ac6b5ff813ba064ffa50c923a92d82ba3cf90b
Juan Jose Alvarez <juan.pfalvarez@gmail.com>
2023-03-25 19:52:37 -0600

    cambio a

--- Proyecto_Final_Sistema_Reservaciones/Pages/Mis_Reservaciones.aspx.designer.cs
+++ Proyecto_Final_Sistema_Reservaciones/Pages/Mis_Reservaciones.aspx.designer.cs
@@ -22,14 +22,5 @@ namespace Proyecto_Final_Sistema_Reservaciones.Pages
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label1;
-
-        /// <summary>
-        /// Control Label2.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label2;
     }
 }
